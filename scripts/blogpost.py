60d41f1c16320ab10722105a22159bff161e1743
creeva <74924448+creeva@users.noreply.github.com>
2023-02-10 13:22:23 -0500

    Revert "test"
    This reverts commit 308142de0e135aa8cd0835e1513b1ae04aaf9fc8.

--- scripts/blogpost.py
+++ scripts/blogpost.py
@@ -6,7 +6,6 @@ for filepath in glob.iglob('../**/index.html', recursive=True):
     s = s.replace('http://10.10.2.102/creevacom', 'https://creeva.com')
     s = s.replace('http://localhost/wordpress', 'https://creeva.com')
     s = s.replace('https://localhost/wordpress', 'https://creeva.com')
-    s = s.replace('<!-- Powered by Staatic (https://staatic.com/) -->', '')
     s = s.replace('<div class="rb row text-center">WordPress Theme by <a href="https://richwp.com/">RichWP</a></div></footer><script type="text/javascript" src="https://creeva.com/wp-content/plugins/simple-share-buttons-adder/js/ssba.js?ver=1676009712" id="simple-share-buttons-adder-ssba-js"></script>', '')
     with open(filepath, "w") as file:
         file.write(s)
