fb8e6ef99def7ce937f9987cdd2e214ac94f4db5
creeva <74924448+creeva@users.noreply.github.com>
2023-02-10 13:25:36 -0500

    Fixed?

--- scripts/blogpost.py
+++ scripts/blogpost.py
@@ -1,1 +1,11 @@
-# test
+import glob
+import os
+for filepath in glob.iglob('../**/index.html', recursive=True):
+    with open(filepath) as file:
+        s = file.read()
+    s = s.replace('http://10.10.2.102/creevacom', 'https://creeva.com')
+    s = s.replace('http://localhost/wordpress', 'https://creeva.com')
+    s = s.replace('https://localhost/wordpress', 'https://creeva.com')
+    with open(filepath, "w") as file:
+        file.write(s)
+#os.system('git commit -am "test"')
