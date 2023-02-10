059e8d2515c802f2a9f09d846ab6a9a881f65eb4
creeva <74924448+creeva@users.noreply.github.com>
2023-02-10 13:22:34 -0500

    Revert "test"
    This reverts commit f7b06bcc30902a210640d88167b961f1e32852e4.

--- scripts/blogpost.py
+++ scripts/blogpost.py
@@ -1,12 +1,1 @@
-import glob
-import os
-for filepath in glob.iglob('../**/index.html', recursive=True):
-    with open(filepath) as file:
-        s = file.read()
-    s = s.replace('http://10.10.2.102/creevacom', 'https://creeva.com')
-    s = s.replace('http://localhost/wordpress', 'https://creeva.com')
-    s = s.replace('https://localhost/wordpress', 'https://creeva.com')
-    s = s.replace('<div class="rb row text-center">WordPress Theme by <a href="https://richwp.com/">RichWP</a></div></footer><script type="text/javascript" src="https://creeva.com/wp-content/plugins/simple-share-buttons-adder/js/ssba.js?ver=1676009712" id="simple-share-buttons-adder-ssba-js"></script>', '')
-    with open(filepath, "w") as file:
-        file.write(s)
-os.system('git commit -am "test"')
+# test
